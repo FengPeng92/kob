40630b473be5e72f7b72dfc8922022bf77129278
FengPeng92 <peng.fen@northeastern.edu>
2022-07-08 15:08:28 -0400

    connect web and backend

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,12 +3,20 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getBotInfo/")
-    public String getBotInfo() {
-        return "hhhh";
+    public Map<String, String> getBotInfo() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Feng");
+        map.put("rating", "1000");
+
+        return map;
+
     }
 }
