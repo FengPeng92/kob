fa735197b6c6f5137bfcea7ca81f5b330db26a2d
FengPeng92 <peng.fen@northeastern.edu>
2022-07-28 15:58:39 -0400

    login and register web ui

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -15,10 +15,10 @@
         </li>
       </ul>
 
-      <ul class="navbar-nav">
+      <ul class="navbar-nav" v-if="$store.state.user.is_login">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            FengPeng
+            {{$store.state.user.username}}
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li>
@@ -26,8 +26,21 @@
               </li>
               <li><hr class="dropdown-divider"></li>
             
-            <li><a class="dropdown-item" href="#">退出</a></li>
+            <li><a class="dropdown-item" href="#" @click="logout">退出</a></li>
           </ul>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav" v-else>
+        <li class="nav-item dropdown">
+          <router-link class="nav-link" :to="{name: 'user_account_login'}" role="button">
+            Login
+          </router-link>
+        </li>
+        <li class="nav-item dropdown">
+          <router-link class="nav-link" :to="{name: 'user_account_register'}" role="button">
+            Register
+          </router-link>
         </li>
       </ul>
     </div>
@@ -39,14 +52,21 @@
 <script>
 import { useRoute } from "vue-router";
 import { computed } from "vue";
+import { useStore } from "vuex";
 
 export default {
     setup() {
         const route = useRoute();
+        const store = useStore();
 
         let route_name = computed(() => route.name);
+
+        const logout = () => {
+            store.dispatch("logout");
+        }
         return {
-            route_name
+            route_name,
+            logout
         }
     }
 }
---
+++ web/src/views/user/account/UserAccountRegisterView.vue
@@ -0,0 +1,21 @@
+<template>
+        <ContentField>
+            Register
+        </ContentField>
+</template>
+
+<script>
+
+import ContentField from '../../../components/ContentField.vue';
+
+export default {
+    components: {
+        ContentField
+    }
+}
+
+
+
+</script>
+
+<style scoped></style> 
